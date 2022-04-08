944cc9a35f353d6917b6a4f0876e3f5093788ce2
arranfw <71518072+arranfw@users.noreply.github.com>
2022-04-08 09:00:02 -0600

    Tailwind (#5)
    * lint, typecheck, deploy
    
    * remove dev
    
    * test deployment **REMOVE**
    
    * add format check
    
    * fix prettier
    
    * deploy depends on format
    
    * add environment variables
    
    * string
    
    * hack?
    
    * hack didn't work, I'll do it manually
    
    * tailwind works
    
    * format

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import tailwindStylesheetUrl from './styles/tailwind.css';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
@@ -14,14 +15,18 @@ export const meta: MetaFunction = () => ({
   viewport: 'width=device-width,initial-scale=1',
 });
 
+export function links() {
+  return [{ rel: 'stylesheet', href: tailwindStylesheetUrl }];
+}
+
 export default function App() {
   return (
-    <html lang='en'>
+    <html lang='en' className='h-full bg-slate-500'>
       <head>
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className='h-full'>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- remix.config.js
+++ remix.config.js
@@ -2,11 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: "arc",
-  server: "./server.js",
-  ignoredRouteFiles: [".*"],
+  serverBuildTarget: 'arc',
+  server: './server.js',
+  ignoredRouteFiles: ['.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "server/index.js",
   // publicPath: "/_static/build/",
+  devServerPort: null,
 };
