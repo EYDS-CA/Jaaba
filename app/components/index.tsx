3ea3d23a049d6b898a1d1f6c53cf6499258c03c6
Cory Atwater <cory@freshworks.io>
2022-04-08 18:36:05 -0400

    job search screen

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -2,3 +2,7 @@ export * from './Spinner';
 export * from './Nav';
 export * from './UserBar';
 export * from './JobApplications';
+export * from './Input';
+export * from './Select';
+//folders
+export * from './search';
