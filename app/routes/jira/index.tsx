1e56d1cc6e60cdb05816ed832e38e4798091781b
Arran Woodruff <arran@freshworks.io>
2022-04-08 18:22:36 -0600

    remove extra GetMetaData fn

--- app/routes/jira/index.tsx
+++ app/routes/jira/index.tsx
@@ -1,10 +1,6 @@
-// TODO: CRUD operations go here
-
 import type { ActionFunction, LoaderFunction } from '@remix-run/node';
-import { btoa } from '@remix-run/node/base64';
 import { manager } from '~/managers';
 
-// /put/post/get/delete RESTful architecture
 export const action: ActionFunction = async ({ request }) => {
   switch (request.method) {
     case 'POST': {
@@ -15,23 +11,5 @@ export const action: ActionFunction = async ({ request }) => {
 };
 
 export const loader: LoaderFunction = async () => {
-  return await GetMetaData();
-};
-
-export const GetMetaData = async () => {
-  const res = await fetch(
-    'https://jaaba.atlassian.net/rest/api/3/issue/createmeta',
-    {
-      method: 'GET',
-      headers: new Headers({
-        Authorization:
-          'Basic ' +
-          btoa(`${process.env.JIRA_USERNAME}:${process.env.JIRA_API_KEY}`),
-      }),
-    },
-  );
-
-  const response = await res.json();
-
-  return { projectId: response.projects[0].id, issueType: '100001' };
+  return await manager.GetMetaData();
 };
