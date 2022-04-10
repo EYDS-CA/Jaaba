2edce3919c01e3854b1cf5c444287e2b9edb152c
Arran Woodruff <arran@freshworks.io>
2022-04-09 19:45:33 -0600

    fix api

--- app/routes/jira/index.tsx
+++ app/routes/jira/index.tsx
@@ -1,11 +1,26 @@
 import type { ActionFunction, LoaderFunction } from '@remix-run/node';
 import { manager } from '~/managers';
+import invariant from 'tiny-invariant';
 
 export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const name = form.get('name');
+  const email = form.get('email');
+  const letter = form.get('letter');
+
+  const errorMessage = 'Please fill out your profile before applying.';
+  invariant(typeof name === 'string', errorMessage);
+  invariant(typeof email === 'string', errorMessage);
+  invariant(typeof letter === 'string', errorMessage);
+
+  const data = await request.formData();
+  const parentId = data.get('parentId') ?? '';
   switch (request.method) {
     case 'POST': {
-      const parentId = '';
-      return await manager.CreateTicket('title', 'body', parentId);
+      return await manager.CreateTicket(
+        { name, email, letter },
+        parentId.toString(),
+      );
     }
   }
 };
