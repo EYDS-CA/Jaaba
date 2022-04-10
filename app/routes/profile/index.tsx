c397700c037ba6e87eda4cd02df3a50dc73f88cd
Arran Woodruff <arran@freshworks.io>
2022-04-10 14:36:50 -0600

    success message when updating profile

--- app/routes/profile/index.tsx
+++ app/routes/profile/index.tsx
@@ -1,4 +1,4 @@
-import { Form } from '@remix-run/react';
+import { Form, useActionData } from '@remix-run/react';
 import type { ActionFunction, LoaderFunction } from '@remix-run/server-runtime';
 import { redirect } from '@remix-run/server-runtime';
 import { json } from '@remix-run/server-runtime';
@@ -18,6 +18,7 @@ interface ActionData {
     name?: string;
     letter?: string;
   };
+  response?: string;
 }
 
 export const action: ActionFunction = async ({ request }) => {
@@ -46,10 +47,14 @@ export const action: ActionFunction = async ({ request }) => {
 
   invariant(existingUser, 'User not found');
 
-  return saveUserProfile({
+  await saveUserProfile({
     profile: { name, letter },
     email: existingUser.email,
   });
+
+  return {
+    response: 'Profile successfully updated',
+  };
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -60,12 +65,13 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function Profile() {
   const user = useUser();
+  const actionData = useActionData();
 
   return (
     <div className='w-full px-7 pt-4'>
       <h1 className='mb-5 text-2xl font-bold'>My Profile</h1>
 
-      <Form method='post'>
+      <Form method='post' className='flex flex-col items-start gap-2'>
         <input type='hidden' name='email' value={user.email} />
         <label htmlFor='profile-name'>Name</label>
         <input
@@ -73,7 +79,7 @@ export default function Profile() {
           name='name'
           id='name'
           defaultValue={user.profile?.name}
-          className='mb-2 block rounded border border-gray-300 px-2 py-1'
+          className='block rounded border border-gray-300 px-2 py-1'
         />
         <label htmlFor='letter'>Cover Letter</label>
         <textarea
@@ -82,9 +88,14 @@ export default function Profile() {
           name='letter'
           id='letter'
           defaultValue={user.profile?.letter}
-          className='mb-2 block rounded border border-gray-300 px-2 py-1'
+          className='block rounded border border-gray-300 px-2 py-1'
         />
         <Button type='submit'>Save</Button>
+        {actionData?.response ? (
+          <p className='inline rounded bg-green-500 px-2 py-1'>
+            {actionData.response}
+          </p>
+        ) : null}
       </Form>
     </div>
   );
